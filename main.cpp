f72ca9c639c59640159df7b2483683e0b6b55534
Dhia510 <dhia.chaabene123@gmail.com>
2022-07-16 18:55:35 +0200

    uart: Test

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "uart.h"
+//#include <iostream>
+
+int main(){
+
+ uart_init();
+	
+	while(1){
+	  //uart_write('Y');
+		
+		//std::cout<<"Hello from embedded C++ \r\n";
+		printf("Hello from embedded C++ \r\n");
+		
+     for(int i=0;i<180000;i++){}
+	}
+}
